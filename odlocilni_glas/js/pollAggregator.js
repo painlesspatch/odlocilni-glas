dff97340e5b6f5f3c6937c849fbb813657fe92ed
painlesspatch <fele.patti@gmail.com>
2026-03-14 18:04:55 +0100

    Update poll data fetching and parsing logic

--- odlocilni_glas/js/pollAggregator.js
+++ odlocilni_glas/js/pollAggregator.js
@@ -1,8 +1,7 @@
 
 export async function loadPolls(){
 
-const response = await fetch("PASTE_GOOGLE_SHEET_API_HERE")
-const text = await response.text()
+const response = await fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vQpWjzIcpLf9PoJ-Q80b4o3RC-sSbVozv18WwS-waLURUetX6spuAJdf2pLRZLFbvA0-66bGzifkFPB/pub?output=csv")
 
 const rows=text.split("\n").slice(1)
 
@@ -13,18 +12,28 @@ rows.forEach(r=>{
 const c=r.split(",")
 
 polls.push({
-date:c[0],
-pollster:c[1],
-sample:Number(c[2]),
+date:c[1],
+pollster:c[2],
+sample:Number(c[3]),
 
 results:{
-SDS:Number(c[3]),
-Svoboda:Number(c[4]),
-SD:Number(c[5]),
-Levica:Number(c[6]),
-NSi:Number(c[7]),
-Others:Number(c[8])
-}
+SDS:Number(c[4]),
+GibanjeSvoboda:Number(c[5]),
+LevicaVesna:Number(c[6]),
+NSiFokusSLS:Number(c[7]),
+SD:Number(c[8]),
+Demokrati:Number(c[9]),
+Resnica:Number(c[10]),
+PiratskaStranka:Number(c[11]),
+SNS:Number(c[12]),
+Prerod:Number(c[13]),
+GlasUpokojencev:Number(c[14]),
+MiSocialisti:Number(c[15]),
+Neodloceni:Number(c[17]),
+Nikogar:Number(c[18]),
+Drugo:Number(c[19]),
+NeZelimOdgovoriti:Number(c[20]),
+PraznaGlasovnica:Number(c[21])}
 
 })
 
